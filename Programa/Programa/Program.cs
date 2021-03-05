142414dfc81cc5887558568535d2bc20f4cb4baa
unknown <gustavorisso2020@gmail.com>
2021-03-05 13:59:44 -0300

    Projeto do VS criado

---
+++ Programa/Programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
